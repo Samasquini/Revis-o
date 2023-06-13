8d177e236c7eac8be7df57a89c6e54bcad71b010
Samasquini <msamasquini321@gmail.com>
2023-06-13 13:27:18 -0300

    Revisao prova

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿try
+{
+    Console.WriteLine("Digite um numero!");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite outro numero!");
+    int b = int.Parse(Console.ReadLine());
+    int result = a / b;
+    Console.WriteLine("O resultado é:"+result);
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Algo de errado não está certo!");
+}
